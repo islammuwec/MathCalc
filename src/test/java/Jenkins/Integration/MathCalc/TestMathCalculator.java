b3a0155cc1dd189b0490e77ef95232d77df04129
Rakib Islam <islamm@uwec.edu>
2020-11-23 17:18:53 -0600

    Adding a test method for deduct

--- src/test/java/Jenkins/Integration/MathCalc/TestMathCalculator.java
+++ src/test/java/Jenkins/Integration/MathCalc/TestMathCalculator.java
@@ -11,4 +11,12 @@ public class TestMathCalculator {
         //Assert.assertEquals(5.00,calc.Sum(2,3));
         Assert.assertTrue(5==calc.Sum(2,3));
     }
+    
+    @Test
+    public void testDedcution() {
+        MathCalculator calc=new MathCalculator();
+
+        //Assert.assertEquals(5.00,calc.Sum(2,3));
+        Assert.assertTrue(5==calc.Deduct(5, 4));
+    }
 }
